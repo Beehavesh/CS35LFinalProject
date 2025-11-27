3bf31cf5f393502b7263005b08e39ff416a1a505
Beehavesh <bhaveshapathak@gmail.com>
2025-11-27 01:29:26 -0800

    saving progress in case of reset

--- Login.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import loginComponent from '../Components/loginComponent';
-
-export default function Login() {
-  return (
-    <div>Login</div>
-  );
-}
--- loginComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function loginComponent() {
-    return (
-        <div>
-            <h1>Login Component</h1>
-        </div>
-    );
-}
