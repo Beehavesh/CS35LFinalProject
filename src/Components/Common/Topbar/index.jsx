2ff495c92725a3ba559abcd5f1f7ae5d89896d82
Beehavesh <bhaveshapathak@gmail.com>
2025-11-28 17:52:58 -0800

    added a topbar

---
+++ src/Components/Common/Topbar/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+export default function Topbar() {
+    return (
+        <div className="topbar-container">
+            <h2>My Application Topbar</h2>
+        </div>
+    );
+}
