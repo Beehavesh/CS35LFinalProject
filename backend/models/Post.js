6a6066f15e216c33fd64a1a14fb1755565942700
Beehavesh <bhaveshapathak@gmail.com>
2025-11-30 01:10:17 -0800

    fixed a model is defined twice error

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    userId: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+  text: String,
+  userId: String,
+  createdAt: { type: Date, default: Date.now }
 });
 
-export default mongoose.model("Post", PostSchema);
+export default mongoose.models.Post || mongoose.model("Post", PostSchema);
