6642f412e8fdccf5fbe1cd59cb4ca88c4a357721
Beehavesh <bhaveshapathak@gmail.com>
2025-11-29 20:42:16 -0800

    Added posts from backend to frontend

--- frontend/src/Components/Common/PostUpdate/index.jsx
+++ frontend/src/Components/Common/PostUpdate/index.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
+import React, { use, useMemo, useState } from 'react';
 import './index.scss';
 import ModalComponent from '../Modal/index.jsx';
 import { getAuth } from "firebase/auth";
 import { toast } from 'react-toastify';
 
+const getPosts = async (setAllPosts) => {
+    try {
+        const auth = getAuth(); 
+        const token = await auth.currentUser.getIdToken();
+        const response = await fetch("http://localhost:5001/api/posts", {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        if (!response.ok) throw new Error("Failed to fetch posts");
+        const data = await response.json();
+        setAllPosts(data);
+    } catch (err) {
+        console.log(err);
+    }   
+};
+
+
 export default function PostStatus() {
     const [modalOpen, setModalOpen] = useState(false);
-    const [status, setStatus] = useState('');
+    const [postStatus, setPostStatus] = useState('');
+    const [allPosts, setAllPosts] = useState([]);
+    
     const sendStatus = async () => { 
-                if (!status.trim()) return;
+                if (!postStatus.trim()) return;
 
         try {
-            const auth = getAuth();
-            const token = await auth.currentUser.getIdToken();
             const response = await fetch("http://localhost:5001/api/posts", {
                 method: "POST",
                 headers: {
@@ -20,19 +39,24 @@ export default function PostStatus() {
                     "Authorization": `Bearer ${token}`,
                 },
                 body: JSON.stringify({
-                    text: status,
+                    text: postStatus,
                 })
             });
 
             if (!response.ok) throw new Error("Failed to post");
                 toast.success("Posted!");
-            setStatus("");
+            setPostStatus("");
             setModalOpen(false);
         } catch (err) {
             console.log(err);
             toast.error("Failed to create post.");
         }
     };
+
+    useMemo(() => {
+        getPosts(setAllPosts);
+    }, []);
+
     return (
         <div className="post-status-container">
             <div className="post-status">
@@ -43,8 +67,15 @@ export default function PostStatus() {
                     Start a Post
                 </button>
             </div>
-            <ModalComponent status={status} setStatus={setStatus} modalOpen={modalOpen} setModalOpen={setModalOpen} sendStatus={sendStatus}/>
-        </div>
-    );
+            <ModalComponent status={postStatus} setStatus={setPostStatus} modalOpen={modalOpen} setModalOpen={setModalOpen} sendStatus={sendStatus}/>
+        {allPosts.map((post) => {
+            return (
+                <div key={post._id} className="post-item">
+                    <p>{post.text}</p>
+                </div>
+            );
+        }
+    )
+    }
+    </div>);
 }
-
