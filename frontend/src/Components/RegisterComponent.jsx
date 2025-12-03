3c5441e8fedc5c10e1c02c9371b65f517be850d6
izzy-u <isabellayoeu@gmail.com>
2025-12-03 11:48:18 -0800

    Side-by-side descriptive landing page and registration page

--- frontend/src/Components/RegisterComponent.jsx
+++ frontend/src/Components/RegisterComponent.jsx
@@ -39,10 +39,14 @@ export default function RegisterComponent() {
     return (
         <>
 
-                <p className="marquee">
+    <div className="entire-page-container">
+    <img src = {motifChrome} className = "linkedoutlogo"/>
+
+        <p className="marquee">
             <span> <x-purpletext> MOTIF </x-purpletext> MADE BY MUSICIANS, FOR MUSICIANS.  <x-purpletext> MOTIF </x-purpletext>  MADE BY MUSICIANS.  <x-purpletext> MOTIF </x-purpletext> MADE BY MUSICIANS, FOR MUSICIANS. <x-purpletext> MOTIF </x-purpletext> MADE BY MUSICIANS, FOR MUSICIANS.  &nbsp;</span>
         </p>
-        <div className = "model-viewer">
+
+            <div className = "model-viewer">
                     <model-viewer
                     id="reveal"
                     loading="eager"
@@ -57,9 +61,25 @@ export default function RegisterComponent() {
                     shadow-intensity="1"
                     alt="3d model of motif logo"
                     ></model-viewer>
+            </div>
+
+        <div className="left-side"> 
+            <div className="hero-wrapper">
+                <div className="hero-divider">
+                    <h1 className="heading1"> MOTIF </h1>
+                    <h2> an innovative platform for musicians, composers, & employers to connect. </h2>
+                    <h3> FEATURING... </h3>
+                    <ul>
+                        <li> PLAYLIST UPLOAD</li>
+                        <li> MUSIC TASTE-BASED JOB SEARCH </li>
+                        <li> VIRTUAL INTERACTIONS </li>
+                    </ul>
                 </div>
-        <div className = "login-wrapper ">
-            <img src = {motifv2} className = "linkedoutlogo"/>
+            </div>
+        </div>
+
+        <div className="left-side">
+        <div className = "login-wrapper">
             <div className = "login-divider">
                 <h1 className="heading1">SIGN UP</h1>
                 <p className="subheading1">Welcome! Enter your info to create an account.</p>
@@ -101,6 +121,9 @@ export default function RegisterComponent() {
             <p className="signup-text">Already have an account? <span className="signup-link" onClick={() => navigate("/")}>Sign in</span></p>
             </div>
         </div>
+        </div>
+
+     </div>
     </>
     );
 }
