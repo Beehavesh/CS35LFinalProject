d83d147324b30c580ca2b7bf2a17bd77f17a22a1
Beehavesh <bhaveshapathak@gmail.com>
2025-12-02 23:12:34 -0800

    resolved the merge conflict #2

--- frontend/src/Components/Common/PostUpdate/index.jsx
+++ frontend/src/Components/Common/PostUpdate/index.jsx
@@ -94,23 +94,5 @@ export default function PostStatus() {
                 </button>
             </div>
             <ModalComponent status={postStatus} setStatus={setPostStatus} modalOpen={modalOpen} setModalOpen={setModalOpen} sendStatus={sendStatus}/>
-<<<<<<< HEAD
-=======
-
->>>>>>> 2648fce340dfad74d0c0b31c4292bba86c313913
-        {allPosts.map((post) => {
-            return (
-                <div key={post._id} className="post-item">
-                    <p>{post.text}</p>
-                    <LikeButton />
-                </div>
-            );
-        })}
-<<<<<<< HEAD
         </div>);
-=======
-        </div>
-    );
-
->>>>>>> 2648fce340dfad74d0c0b31c4292bba86c313913
 }
