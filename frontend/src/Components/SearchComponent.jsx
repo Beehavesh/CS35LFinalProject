04ef78dbdc070b9f067b3ed07283f5e8344b3038
izzy-u <isabellayoeu@gmail.com>
2025-12-05 11:40:07 -0800

    Search page built to match rest of pages in design

--- frontend/src/Components/SearchComponent.jsx
+++ frontend/src/Components/SearchComponent.jsx
@@ -6,7 +6,8 @@ export default function SearchComponent() {
     return (
         <div>
             <Topbar />
-            <h1>Search</h1>
+            <h1 className="heading2">SEARCH!</h1>
+            <h3> Looking for something? Enter a search query. </h3>
             <Search />
         </div>
     );
