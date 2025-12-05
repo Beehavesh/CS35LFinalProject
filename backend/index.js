8138b053e53d6ecff749a467541f2f72a8d2efa6
Beehavesh <bhaveshapathak@gmail.com>
2025-12-04 20:01:43 -0800

    attempted to fix modularization merge bug #3

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/posts", postRoutes);
-app.use("/api", likeRoutes);
+//app.use("/api", likeRoutes);
 app.use("/api", playlistRoutes);
 
 // Render backend
